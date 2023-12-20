2cc847c46ae58da0750a8f2602838ce039b6bc8e
agostinhooliv <agostinhooliv@hotmail.com>
2023-12-20 11:06:12 -0300

    Add ExceptionHandler

--- src/main/java/br/com/agostinho/algafood/api/controller/CidadeController.java
+++ src/main/java/br/com/agostinho/algafood/api/controller/CidadeController.java
@@ -4,12 +4,14 @@ import br.com.agostinho.algafood.domain.model.Cidade;
 import br.com.agostinho.algafood.domain.model.Estado;
 import br.com.agostinho.algafood.domain.service.CidadeService;
 import br.com.agostinho.algafood.domain.service.EstadosService;
+import br.com.agostinho.algafood.exceptions.ValidationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -116,5 +118,11 @@ public class CidadeController {
 
             ReflectionUtils.setField(field, cidadeDestino, novoValor);
         });
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> tratarEstadoNaoEncontradoException(ValidationException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(String.format("Testando ExceptionHandler"));
     }
 }
